c7fe170354e36230bd2f619a946c3f8daf9447bf
tess3rac7 <tess3rac7@protonmail.com>
2023-01-03 17:02:56 -0500

    Update scripts.

--- hardhat.config.js
+++ hardhat.config.js
@@ -42,7 +42,7 @@ module.exports = {
   },
   networks: {
     mainnet: {
-      url: `https://rpc.ftm.tools`,
+      url: `https://rpc.ankr.com/fantom`,
       chainId: 250,
       accounts: [`0x${PRIVATE_KEY}`],
     },
