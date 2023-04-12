6d22052c5d444ba5517e9321afdbc30274417821
Zokunei <zokunei@bytemasons.com>
2023-04-12 12:41:16 -0400

    update for new deployment

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -12,6 +12,7 @@ async function deployVault(wantAddress, wantSymbol) {
     '0x1E71AEE6081f62053123140aacC7a06021D77348',
     '0x81876677843D00a7D792E1617459aC2E93202576',
     '0x1A20D7A31e5B3Bc5f02c8A146EF6f394502a10c4',
+    '0x60BC5E0440C867eEb4CbcE84bB1123fad2b262B1',
   ];
   const multisigRoles = [
     '0x04C710a1E8a738CDf7cAD3a52Ba77A784C35d8CE', // super admin
